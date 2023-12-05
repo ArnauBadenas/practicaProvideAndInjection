e76fea43cedea49388cc4701d957c053917fb77f
ArnauBadenas <4bvives@gmail.com>
2023-12-05 19:56:30 +0100

    poner template al principio porque me da toc

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
-<script setup lang="ts">
-import { type Ref, ref, provide } from 'vue';
-import type { Order } from './types/Order';
-import order from './components/order.vue';
-import type { Currency } from './types/Currency';
-const orders: Ref<Order[]> = ref([{ name: "Hamburger 🍔.", price: 5 },
-{ name: "Cheeseburger 🧀", price: 6 },
-{ name: "Impossible Burger 🥕", price: 7 },
-{ name: "Fries 🍟", price: 2 }])
-
-const orderTitle: Ref<string> = ref("")
-const cart: Ref<Order[]> = ref([])
-
-function submitOrder(order: Order) {
-  cart.value.push(order)
-}
-function totalPrice(): number {
-  let total = 0
-  for (let item of cart.value) {
-    total += item.price
-  }
-  return total
-}
-
-//provide currency
-let currency: Ref<Currency> = ref("€")
-provide<Ref<Currency>>('currency', currency) //TODO type
-</script>
-
 <template>
   <header>
-
   </header>
   <main>
     <section id="restaurant">
       <article class="orders">
         <h1>Tom's Dinner</h1>
       </article>
-
       <img
         src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/19d15c51-f7c6-4914-838c-06e5599f84b6/dfrv9yk-d36c8c4e-9502-43b0-b41a-6077aa7378bf.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE5ZDE1YzUxLWY3YzYtNDkxNC04MzhjLTA2ZTU1OTlmODRiNlwvZGZydjl5ay1kMzZjOGM0ZS05NTAyLTQzYjAtYjQxYS02MDc3YWE3Mzc4YmYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.uCioa3ijsWL19j-TA8qJm1cUMBTO1lBYbOfV8j0geSw"
         alt="">
@@ -76,16 +45,42 @@ provide<Ref<Currency>>('currency', currency) //TODO type
           <td>{{ totalPrice() + ((totalPrice() / 100) * 21) }}€</td>         
         </tr>
       </table>
-
-        
-
-
     </section>
   </main>
   <footer>
 
   </footer>
 </template>
+
+<script setup lang="ts">
+import { type Ref, ref, provide } from 'vue';
+import type { Order } from './types/Order';
+import order from './components/order.vue';
+import type { Currency } from './types/Currency';
+const orders: Ref<Order[]> = ref([{ name: "Hamburger 🍔.", price: 5 },
+{ name: "Cheeseburger 🧀", price: 6 },
+{ name: "Impossible Burger 🥕", price: 7 },
+{ name: "Fries 🍟", price: 2 }])
+
+const orderTitle: Ref<string> = ref("")
+
+const cart: Ref<Order[]> = ref([])
+
+function submitOrder(order: Order) {
+  cart.value.push(order)
+}
+function totalPrice(): number {
+  let total = 0
+  for (let item of cart.value) {
+    total += item.price
+  }
+  return total
+}
+
+//provide currency
+let currency: Ref<Currency> = ref("€")
+provide<Ref<Currency>>('currency', currency) //TODO type
+</script>
 
 <style scoped>
 .orders {
